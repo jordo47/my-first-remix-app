49247c0ceb20e2d72f3e351bce8a9987c44e15e0
jordo47 <jordojordo47@gmail.com>
2024-01-22 11:57:43 -0600

    1/22 devlog

---
+++ app/routes/_index.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+    return (
+        <p id="index-page">
+            This is a demo for REMIX.
+            <br />
+            Check out{" "}
+            <a href="https://remix.run">the docs at remix.run</a>
+        </p>
+    )
+}
